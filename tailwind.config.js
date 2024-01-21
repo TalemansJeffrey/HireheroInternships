3f5c4185de9334ab69e117c6bad2820b67fc8851
Cheffren <jeffrey.veyron@gmail.com>
2024-01-21 17:15:48 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,51 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'
+  ],
+  theme: {
+    screens: {
+      sm:'480px',
+        md:'768px',
+        lg:'976px',
+        xl:'1440px'
+    },
+    extend: {
+      fontFamily: {
+        'epilogue': ['Epilogue', 'sans-serif']  
+      },
+      fontWeight: {
+        'bolder' : '700'
+
+      },
+      height: {
+        '112': '28rem',
+        '128': '32rem',
+        '144': '36rem',
+        '160': '40rem',
+        '176': '44rem'
+
+      },
+      width: {
+        '112': '28rem',
+        '128': '32rem',
+        '144': '36rem',
+        '160': '40rem',
+        '176': '44rem'
+      },
+      
+    
+      colors: {
+        darkBlue: "#97CAF9",
+        lightBlue:"#C4E4F7",
+        purple: "#9080FC",
+        orange: "#FFDAB9",
+        green: "#18DAC3",
+        red: "#FA8072",
+        black: "#111111",
+        white: "#F3EFEF"
+      }
+    },
+  },
+  plugins: [],
+}
+
